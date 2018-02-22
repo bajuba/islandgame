9f69b4dd05f12137bb67f0aa9188db5b6665491c
bajuba <bajuba@hotmail.com>
2018-02-21 20:46:38 -0600

    added new files

---
+++ assets/ocean.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ocean" tilewidth="16" tileheight="16" tilecount="100" columns="10">
+ <image source="island.png" width="160" height="160"/>
+</tileset>
